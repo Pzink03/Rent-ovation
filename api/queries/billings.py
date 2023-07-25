5edb52d9bb4314c6550befc354fcff1b17c825da
Karuna Adhikari <KaAdhikari@luxoft.com>
2023-07-25 14:30:37 -0400

    billings

--- api/queries/billings.py
+++ api/queries/billings.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel
 from typing import Union, List
+from datetime import date
 from queries.pool import pool
 import traceback
 
@@ -9,10 +10,9 @@ class Error(BaseModel):
 
 
 class BillingsIn(BaseModel):
-    id: int
     name: str
     card_number: int
-    expirydate: int
+    expirydate: date
     cvv: int
 
 
@@ -20,37 +20,39 @@ class BillingsOut(BaseModel):
     id: int
     name: str
     card_number: int
-    expirydate: int
+    expirydate: date
     cvv: int
 
+class BillingsUpdate(BaseModel):
+    id: int
+    name: str
+    card_number: int
+    expirydate: date
+    cvv: int
 
 class BillingsRepository:
-    def create_billings(
-        self, billings: BillingsIn
-    ) -> Union[BillingsOut, Error]:
+    def create_billings(self, billings: BillingsIn) -> Union[BillingsOut, Error]:
         try:
             with pool.connection() as conn:
                 with conn.cursor() as db:
-                    db.execute(
+                    result = db.execute(
                         """
-                        INSERT INTO billing
-                            (id, name, card_number, expirydate, cvv)
+                        INSERT INTO billings
+                            ( name, card_number, expirydate, cvv)
                         VALUES
-                            (%s,%s,%s,%s,%s)
+                            (%s,%s,%s,%s)
                         RETURNING id;
                         """,
 
                         [
-                            billings.id,
                             billings.name,
-                            billings.card_number,
+                            billings.card_number,                        
                             billings.expirydate,
                             billings.cvv,
-
                          ],
                     )
-                    result = db.fetchone()
-                    id = str(result[0])
+        
+                    id = result.fetchone()[0]
                     return BillingsOut(
                         id=id,
                         name=billings.name,
@@ -63,19 +65,83 @@ class BillingsRepository:
             return Error(message="Create billings failed")
 
 
-    def get_all_billings(self) -> List[BillingsOut]:
+    def get_all_billings(self) -> Union[Error, List[BillingsOut]]:
+        try:
             with pool.connection() as conn:
                 with conn.cursor() as db:
-                    result = db.execute(
+                    db.execute(
                         """
-                        SELECT id 
-                            , name
-                            , card_number
-                            , expirydate
-                            , cvv
-                            FROM billings
-                            ORDER BY id;
-                            """
+                        SELECT id
+                        , name
+                        , card_number
+                        , expirydate
+                        , cvv
+                        FROM billings
+                        ORDER BY id;
+                        """
                     )
-                    result = db.fetchall()  
-            return [self.record_to_billings_out(record) for record in result]
+                    return [
+                        BillingsOut(
+                            id=record[0],
+                            name=record[1],
+                            card_number=record[2],
+                            expirydate=record[3],
+                            cvv=record[4],
+                        )
+                        for record in db.fetchall() 
+                    ]
+        except Exception:
+            traceback.print_exc()
+            return Error(message="Getting billings failed")
+        
+    def update_billings(self, billings_id: int, billings: BillingsIn) -> Union[BillingsOut, Error]:
+        try:
+            with pool.connection() as conn:
+                with conn.cursor() as db:
+                    db.execute(
+                    """
+                        UPDATE billings
+                        SET name = %s
+                        , card_number = %s
+                        , expirydate = %s
+                        , cvv = %s
+                        WHERE id = %s
+                    """,
+                    [
+                        billings.name,
+                        billings.card_number,
+                        billings.expirydate,
+                        billings.cvv,
+                        billings_id,
+                    ]
+                )
+                return BillingsUpdate(
+                    id=billings_id,
+                    name=billings.name,
+                    card_number=billings.card_number,
+                    expirydate=billings.expirydate,
+                    cvv=billings.cvv
+                )
+        except Exception:
+            traceback.print_exc()
+            return Error(message="Update user billings failed")
+
+    def delete_a_billings(self, billings_id: int) -> bool:
+        try:
+            with pool.connection() as conn:
+                with conn.cursor() as db:
+                    db.execute(
+                        """
+                        DELETE FROM billings
+                        WHERE id = %s
+                        """,
+                        [billings_id]
+                    )
+                return True
+        except Exception:
+            traceback.print_exc()
+            return False
+
+
+
+
--- api/routers/billings.py
+++ api/routers/billings.py
@@ -1,31 +1,46 @@
 import os
 from fastapi import APIRouter, Depends
-from typing import List, Dict
+from typing import Union, List
 from queries.billings import (
+    Error,
     BillingsIn,
     BillingsOut,
+    BillingsUpdate,
     BillingsRepository,
 )
 from authenticator import AccountAuthenticator
-
 router = APIRouter()
 
 
-@router.post("/billings", response_model= BillingsOut)
+@router.post("/billings", response_model= Union[BillingsOut, Error])
 def create_billings(
     billings: BillingsIn,
     repo: BillingsRepository = Depends(),
+    
 ):
     return repo.create_billings(billings)
     
 
 
 @router.get(
-    "/billings/",
-    response_model= Dict[str, List[BillingsOut]])
+    "/billings/", response_model=Union[Error, List[BillingsOut]])
 def get_all_billings(
     repo: BillingsRepository = Depends(),
 ):
-     billings = repo.get_all_billings()
-     return {"billings": billings}
+     return repo.get_all_billings()
+
+@router.put("/billings/{billings_id}", response_model = BillingsUpdate)
+def update_billings(
+    billings_id: int,
+    billings: BillingsIn,
+    repo: BillingsRepository = Depends(),
+) -> BillingsUpdate:
+    return repo.update_billings(billings_id, billings)
+
+@router.delete("/billings/{billings_id}", response_model=bool)
+def delete_billings(
+    billings_id: int,
+    repo: BillingsRepository = Depends(),
+) -> bool:
+    return repo.delete_a_billings(billings_id)
 
